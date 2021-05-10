7fa39c1d6b6fb60da4f223ab954e4d8c1037f1a9
netgator@163.com <netgator@163.com>
2021-05-10 09:21:08 +0800

    Initial commit for rustlings(v4.4.0) solution

---
+++ exercises/move_semantics/move_semantics2.rs
@@ -0,0 +1,30 @@
+// move_semantics2.rs
+// Make me compile without changing line 13!
+// Execute `rustlings hint move_semantics2` for hints :)
+
+fn main() {
+    let mut vec0 = Vec::new();
+    
+    vec0.push(11);
+
+    //let mut vec1 = fill_vec(&vec0);
+    fill_vec(&mut vec0);
+
+    // Do not change the following line!
+    println!("{} has length {} content `{:?}`", "vec0", vec0.len(), vec0);
+
+    //vec1.push(88);
+
+    //println!("{} has length {} content `{:?}`", "vec1", vec1.len(), vec1);
+}
+
+//fn fill_vec(vec: &Vec<i32>) -> Vec<i32> {
+fn fill_vec(vec: &mut Vec<i32>) {
+    //let mut vec1 = vec.clone();
+
+    vec.push(22);
+    vec.push(44);
+    vec.push(66);
+
+    //vec.to_vec()
+}
